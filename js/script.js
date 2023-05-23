51d091b0f349aa6a4ad685916d47df2ebdba5723
Felix-Andersson <felix.andersson3@elev.ga.ntig.se>
2023-05-23 14:47:31 +0200

    BIIG UPDATE

--- js/script.js
+++ js/script.js
@@ -4,9 +4,14 @@ const ctx = canvas.getContext("2d");
 // Random
 const header = document.getElementById("header");
 var start = Date.now();
+var setNormal = true;
 
 // Ljud
 const bolibompa_theme = document.getElementById('bolibompa_theme');
+const press_t = document.getElementById('press_t');
+const drawer_sound = document.getElementById('grattis');
+const drunknar = document.getElementById('drunknar');
+const mimimi = document.getElementById('mimimi');
 
 // Animation
 const bengtSpriteSheet = document.getElementById('bengt_sprite_sheet');
@@ -16,9 +21,66 @@ var spriteFrame = 0;
 
 // Konstanter
 const house1 = document.getElementById('house1_sprite_sheet');
-const house2 = document.getElementById('house2_sprite_sheet')
+const house2 = document.getElementById('house2_sprite_sheet');
+const grass_background = document.getElementById('grass_background');
+const house1_inside = document.getElementById('inside_house1');
+const bengt_bild = document.getElementById('bengt_bild');
+const house2_inside = document.getElementById('inside_house2');
+const bengt_drown = document.getElementById('bengt_drown');
+
+const door1 = {
+    width: 11,
+    height: 5,
+    x: 138,
+    y: 50
+}
+
+const door2 = {
+    width: 11,
+    height: 5,
+    x: 276,
+    y: 158
+}
+
+const drawer = {
+    width: 100,
+    height: 60,
+    x: 50,
+    y: 20
+}
+
+const river = {
+    width: 136,
+    height: 50,
+    x: 0,
+    y: 220
+}
 
 function animate() {
+    if (scene === 0) {
+        if (collides(bengt, river)) {
+            bengt.x = 45;
+            bengt.y = 235;
+            ctx.drawImage(bengt_drown, bengt.x, bengt.y);
+            header.innerHTML = "Inside Drown"
+            toggleSound(drunknar);
+            return;
+        }
+    } else if (scene === 3) {
+        if (collides(bengt, river)) {
+            bengt.x = 26;
+            bengt.y = 223;
+            ctx.save();
+            ctx.translate(bengt.x + bengt.width / 2, bengt.y + bengt.height / 2);
+            ctx.rotate(-Math.PI / 2);
+            ctx.translate(-bengt.x - bengt.width / 2, -bengt.y - bengt.height / 2);
+            ctx.drawImage(bengt_drown, bengt.x, bengt.y, bengt.width, bengt.height);
+            ctx.restore();
+            header.innerHTML = "Inside Bed"
+            toggleSound(mimimi);
+            return;
+        }
+    }
     if ((bengt.dx != 0) || (bengt.dy != 0)) {
         if ((stagger % staggerFrame) == 0) {
             if (spriteFrame < 3) {
@@ -75,16 +137,15 @@ const bengt = {
     y: 80,
     dx: 0,
     dy: 0,
+    width: 32,
+    height: 48,
 
     name: "Bengt",
     reverse: false,
-    inventory: [0, 2, 3, 4] //lägg in: items.speed_potion
+    inventory: [] //lägg in: items.speed_potion
 }
 
-const scenes = {
-    main: 0,
-    house1: 1
-}
+var scene = 0;
 
 // Kontroller konstant som sparar alla tryckningar (utan detta kan inte två knappar tryckas samtidigt)
 const controller = {
@@ -105,7 +166,18 @@ function togglePlay(song) {
     }
 }
 
-var setNormal = true;
+function toggleSound(sound) {
+    sound.play();
+}
+
+// Kolla efter kollision mellan två objekt med hjälp av "axis-aligned bounding box (AABB)"
+// Källa: https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+function collides(obj1, obj2) {
+    return obj1.x < obj2.x + obj2.width &&
+        obj1.x + obj1.width > obj2.x &&
+        obj1.y < obj2.y + obj2.height &&
+        obj1.y + obj1.height > obj2.y;
+}
 
 function speedWalk() {
     i = 0
@@ -113,7 +185,7 @@ function speedWalk() {
     while (i < bengt.inventory.length) {
         if (bengt.inventory[i] === items.speed_potion) {
             items.speed_potion[0] = 5;
-            staggerFrame = 1;
+            staggerFrame = 2;
             header.innerHTML = "speed haha waow";
             setNormal = false;
             break;
@@ -175,6 +247,37 @@ function walkRight() {
     }
 }
 
+function screenCollide() {
+    if (bengt.x > (360-bengt.width)) {
+        bengt.x = 360-bengt.width;
+    } else if (bengt.x < 0) {
+        bengt.x = 0;
+    }
+
+    if (bengt.y > (270-bengt.height)) {
+        bengt.y = 270-bengt.height;
+    } else if (bengt.y < 0) {
+        bengt.y = 0;
+    }
+}
+
+// function houseCollide() {
+    // if (bengt.width > ())
+// }
+
+function enterDoor() {
+    if (collides(bengt, door1)) {
+        scene = 1;
+        bengt.x = 100;
+        bengt.y = 180;
+    }
+
+    if (collides(bengt, door2)) {
+        scene = 3;
+        bengt.x = 300;
+        bengt.y = 180;
+    }
+}
 
 document.addEventListener('keydown', function(e) {
     if (controller[e.key]) {
@@ -191,6 +294,12 @@ document.addEventListener('keydown', function(e) {
     } else if (e.key === 'r') {
         bengt.x = 100;
         bengt.y = 80;
+        scene = 0;
+    } else if (e.key === 't') {
+        if (collides(bengt, drawer)) {
+            toggleSound(drawer_sound);
+            scene = 2;
+        }
     }
 });
 document.addEventListener('keyup', function(e) {
@@ -201,20 +310,53 @@ document.addEventListener('keyup', function(e) {
 // tar elementen i kontroller konstanten och använder Foreach loop för att utföra funktionerna de är associerade med
 const executeMoves = () => {Object.keys(controller).forEach(key=> {controller[key].func()})}
 
-function interact(items) {
-    document.add
+function interact() {
+    
 }
 
 function loop() {
     requestAnimationFrame(loop); //säger till browsern att jag vill utföra en animation och tillkallar en funktion för att uppdatera animationen innan nästa ommålning av canvasen
     ctx.clearRect(0,0,canvas.width,canvas.height); //gör hela canvasen tom
+    if (scene === 0) {
+        ctx.drawImage(grass_background, 0, 0);
+        enterDoor();
+        ctx.drawImage(house1, 250, 120)
+        ctx.drawImage(house2, 50, 20)
+
+    } else if (scene === 1) {
+        ctx.drawImage(house1_inside, 0, 0);
+        if (collides(bengt, drawer)) {
+            ctx.fillStyle = "white";
+            ctx.fillRect(76, 8, 44, 15);
+            ctx.fillStyle = "black";
+            ctx.rect(76, 8, 44, 15);
+            ctx.stroke();
+            ctx.font = "12px comic sans";
+            ctx.fillText("Press T", 80, 20);
+            toggleSound(press_t);
+        }
+    } else if (scene === 2) {
+        ctx.drawImage(bengt_bild, 0, 0)
+        if (drawer_sound.paused) {
+            scene = 0;
+            togglePlay(bolibompa_theme);
+            const index = bengt.inventory.indexOf(items.speed_potion);
+            if (index > -1) { // only splice array when item is found
+                bengt.inventory.splice(index, 1); // 2nd parameter means remove one item only
+            } else {
+                bengt.inventory.push(items.speed_potion) // samma som append
+            }
+        }
+    } else if (scene === 3) {
+        ctx.drawImage(house2_inside, 0, 0)
+    }
     executeMoves();
-    ctx.drawImage(house1, 250, 120)
-    ctx.drawImage(house2, 50, 20)
-    
-    
+    screenCollide();
     animate();
     timer();
+
+    // ctx.fillRect(138, 50, 11, 5); DOOR1
+    // ctx.fillRect(276, 158, 11, 5); DOOR2
     
 
 
